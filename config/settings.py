f5f25da0ded255ce68c27936c25df5f52fb4f3f0
kuum97 <kuum97@icloud.com>
2023-03-19 15:07:51 +0900

    added booking, category, media, review and wishlist models

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,12 @@ CUSTOM_APPS = [
     "common.apps.CommonConfig",
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
+    "experiences.apps.ExperiencesConfig",
+    "categories.apps.CategoriesConfig",
+    "reviews.apps.ReviewsConfig",
+    "wishlists.apps.WishlistsConfig",
+    "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
 ]
 
 SYSTEM_APPS = [
